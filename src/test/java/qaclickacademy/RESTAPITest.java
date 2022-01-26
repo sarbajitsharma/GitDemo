25014e4b0cced4ff2c482a61d7ef9fce08a668cb
sarbajit <sarbajit.sharma@gmail.com>
2022-01-26 13:26:26 +0530

    Architect Asian development

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -22,4 +22,10 @@ public class RESTAPITest {
 	{
 		System.out.println("deleteTwitter");	
 	}
+	
+	@Test
+	public void deleteTwitter12()
+	{
+		System.out.println("deleteTwddtter");	
+	}
 }
--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -19,5 +19,10 @@ public class SeleniumTest {
 		System.out.println("elementsUi");	
 	}
 	
+	@Test
+	public void elementsUi1()
+	{
+		System.out.println("elementsUi");	
+	}
 	
 }
